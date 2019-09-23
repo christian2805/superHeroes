ef093c98fb6351bd5caece78ea693d70eda07365
Chriss <=>
2019-09-23 12:33:13 +0200

    post and add, working

--- super.js
+++ super.js
@@ -1,5 +1,5 @@
 // 5d88749efd86cb75861e2600
-get();
+
 function get() {
   fetch("https://frontend3-17ab.restdb.io/rest/supa", {
     method: "get",
@@ -11,13 +11,62 @@ function get() {
   })
     .then(e => e.json())
     .then(heroes => {
-      heroes.forEach(hero => {
-        const template = document.querySelector("template").content;
-        const copy = template.cloneNode(true);
-        copy.querySelector("h1").textContent = hero.name;
-        copy.querySelector("h2").textContent = hero.realname;
-        copy.querySelector("p").textContent = hero.powers;
-        document.querySelector("#app").appendChild(copy);
-      });
+      heroes.forEach(addHeroesToTheDom);
+      console.log(heroes);
     });
 }
+get();
+function addHeroesToTheDom(hero) {
+  const template = document.querySelector("template").content;
+  const copy = template.cloneNode(true);
+  copy.querySelector("h1").textContent = hero.name;
+  copy.querySelector("h2").textContent = hero.realname;
+  copy.querySelector("p").textContent = hero.powers;
+  copy.querySelector(".age").textContent = hero.age;
+  document.querySelector("#app").appendChild(copy);
+}
+
+function post() {
+  const data = {
+    name: "The Poopman",
+    realname: "johnnyBoi",
+    powers: "SuperSleep\nSuperPoop",
+    age: 2
+  };
+
+  const postData = JSON.stringify(data);
+  fetch("https://frontend3-17ab.restdb.io/rest/supa", {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      "x-apikey": " 5d88749efd86cb75861e2600",
+      "cache-control": "no-cache"
+    },
+    body: postData
+  })
+    .then(e => e.json())
+    .then(data => {
+      console.log(data);
+      addHeroesToTheDom(data);
+    });
+}
+document.querySelector(".adding").addEventListener("click", e => {
+  post();
+});
+// document.querySelector(".del").addEventListener("click", e => {
+//   deleteHero(hero);
+// });
+// function deleteHero(id) {
+//   console.log("wat");
+//   fetch("https://frontend3-17ab.restdb.io/rest/supa", {
+//     method: "delete",
+//     headers: {
+//       "Content-Type": "application/json; charset=utf-8",
+//       "x-apikey": " 5d88749efd86cb75861e2600",
+//       "cache-control": "no-cache"
+//     }
+//   })
+//     .then(res => res.json())
+//     .then(data => console.log(data));
+//   console.log("delete");
+// }
